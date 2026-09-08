40491f14f5e5acc7637e6ea021bce8221ae04ee3
Aditya Pradhan <adityapradhan0770@gmail.com>
2026-09-08 14:41:05 +0530

    feat: configure project URL patterns and static/media file serving routes

--- backend/jobportal/jobportal/urls.py
+++ backend/jobportal/jobportal/urls.py
@@ -21,11 +21,14 @@ from applications import views
 from django.conf import settings
 from django.conf.urls.static import static 
 
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('jobs/', include('jobs.urls')),
     path('applications/', include('applications.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
-if settings.DEBUG: urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
